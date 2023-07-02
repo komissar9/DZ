bdadbee52decda313924b8bb857377ba2803f045
Alena <magii.alena@yandex.ru>
2023-07-02 09:50:41 +0300

    пром вариант

--- DZ.cs
+++ DZ.cs
@@ -1,4 +1,4 @@
-﻿// // 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+﻿// 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 // Console.Write("Введите трёхзначное число: ");
 // int numberUser = Convert.ToInt32(Console.ReadLine());
 // if (numberUser >=100 && numberUser <1000)
@@ -10,31 +10,27 @@
 //     Console.Write($"Число {numberUser} не трёхзначное");
 
 // 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. До 10 символов
-Console.Write("Введите число, содержащее до 10 символов: ");
-string? numberUser = Console.ReadLine();
-int numberint;
-bool number = int.TryParse(numberUser, out numberint);
-if (number)
-{
-    if (numberUser.Length <= 2)
-    {
-        Console.WriteLine($"В числе {numberUser} нет третьей цифры");
-    }
-    if (numberUser.Length > 2)
-    {
-        Console.WriteLine($"В числе {numberUser} третья цифра {numberUser[2]}");
-    }
-}
-  if (numberUser.Length > 10)
-{
-    Console.WriteLine("Число содержит более 10 цифр");
-}
-else
-{
-    Console.WriteLine("Требуется ввести число");
-}
+// Console.Write("Введите число, содержащее до 10 символов: ");
+// string? numberUser = Console.ReadLine();
+// int numberint;
+// bool number = int.TryParse(numberUser, out numberint);
+// if (number)
+// {
+//     if (numberUser.Length <= 2)
+//     {
+//         Console.WriteLine($"В числе {numberUser} нет третьей цифры");
+//     }
+//     if (numberUser.Length > 2)
+//     {
+//         Console.WriteLine($"В числе {numberUser} третья цифра {numberUser[2]}");
+//     }
+// } 
+// else
+// {
+//     Console.WriteLine("Требуется ввести число");
+// }
 
-// // 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+// 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
 // Console.Write("Введите число дня недели, чтобы проверить выходной это или нет: ");
 // int numberDay = Convert.ToInt32(Console.ReadLine());
 // if (numberDay <= 5)
@@ -52,25 +48,17 @@ else
 
 // Доп. Назовём число «интересным» если его произведение цифр делится на их сумму БЕЗ остатка. Напишите программу, 
 // которая заполняет массив на 10 «интересных» случайных целых чисел от 10 до 1000(999 - последнее). (каждый эл-т массива – сгенерирован случайно)
-// int Number = new Random().Next(10, 1000); 
-// int firstDigital =  Number / 100;
-// int secondDigital = (Number % 100 - Number % 10) / 10;
-// int thirdDigital = (Number % 10);
-// int unusialNumber = (firstDigital * secondDigital * thirdDigital) % (firstDigital + secondDigital + thirdDigital);
-// if (unusialNumber==0)
-// {
-//     int [] unusialNumbers = new int [10] {unusialNumber, unusialNumber, unusialNumber, unusialNumber, unusialNumber, unusialNumber,unusialNumber, unusialNumber, unusialNumber, unusialNumber};
-    // void unusialNumbers(int[] collection) 
-    // {
-    //     int length = collection.Length;
-    //     int index = 0;
-       
-    //     while (index < length)
-    //     {
-    //         collection[index] = new Random().Next(1, 1000);
-    //         index++;
-
-    //     }
-    // }
-//    Console.WriteLine("[{0}]", string.Join(", ", unusialNumbers));
-// }
+int Number = new Random(); 
+int firstDigital =  Number / 100;
+int secondDigital = (Number % 100 - Number % 10) / 10;
+int thirdDigital = (Number % 10);
+int unusialNumber = (firstDigital * secondDigital * thirdDigital) % (firstDigital + secondDigital + thirdDigital);
+if (unusialNumber==0)
+{
+    int[] unusialNumbers = new int[10];
+    for ( int i = 0; i < unusialNumbers.Length; i++)
+    {
+        unusialNumbers[i] = unusialNumber.Next(10, 1000);
+    }
+}
+Console.WriteLine(unusialNumbers[10]);
